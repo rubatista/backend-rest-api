57e105dc6621fa6aab065eba98cc905c8e707ec6
rubatista <inforubenbaptista@gmail.com>
2025-04-10 15:38:22 +0100

    feat: install mongoose and configure him to mongoDB

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
 import compression from 'compression';
 import cors from 'cors';
+import mongoose from 'mongoose';
 
 const app = express();
 
@@ -21,3 +22,9 @@ const server = http.createServer(app);
 server.listen(8080, () => {
     console.log('Server running on http://localhost:8080/');
 })
+
+const MONGO_URL = 'mongodb+srv://root:root@cluster0.05po0xy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+
+mongoose.Promise = Promise;
+mongoose.connect(MONGO_URL);
+mongoose.connection.on('error', (error: Error) => console.log(error));
