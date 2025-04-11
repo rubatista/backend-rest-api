1ee8f6ec7efe3a1d041f4c0233c00652dbd98a88
rubatista <inforubenbaptista@gmail.com>
2025-04-11 18:48:36 +0100

    faeat: create router for authentication and updated express to 5^

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import cookieParser from 'cookie-parser';
 import compression from 'compression';
 import cors from 'cors';
 import mongoose from 'mongoose';
+import router from './router';
 
 const app = express();
 
@@ -19,8 +20,8 @@ app.use(bodyParser.json());
 const server = http.createServer(app);
 
 
-server.listen(8080, () => {
-    console.log('Server running on http://localhost:8080/');
+server.listen(3000, () => {
+    console.log('Server running on http://localhost:3000/');
 })
 
 const MONGO_URL = 'mongodb+srv://root:root@cluster0.05po0xy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
@@ -28,3 +29,5 @@ const MONGO_URL = 'mongodb+srv://root:root@cluster0.05po0xy.mongodb.net/?retryWr
 mongoose.Promise = Promise;
 mongoose.connect(MONGO_URL);
 mongoose.connection.on('error', (error: Error) => console.log(error));
+
+app.use('/', router());
