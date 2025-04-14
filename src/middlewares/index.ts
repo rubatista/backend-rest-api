bc16be3085e1db1fedfaf4ea49be991d83eb7add
rubatista <inforubenbaptista@gmail.com>
2025-04-14 12:24:01 +0100

    feat: created delete controller and his middleware

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -25,3 +25,22 @@ export const isAuthenticated = async (req: express.Request, res: express.Respons
         return res.sendStatus(400);
     }
 }
+
+export const isOwner = async (req: express.Request, res: express.Response, next: express.NextFunction) => {
+    try {
+        const { id } = req.params;
+        const currentUserId = get(req, 'identity._id') as unknown as string;
+
+        if(!currentUserId) {
+            return res.sendStatus(403);
+        }
+
+        if(currentUserId.toString() !== id){
+            return res.sendStatus(403);
+        }
+        next();
+    } catch (error) {
+        console.log(error);
+        return res.sendStatus(400);
+    }
+}
